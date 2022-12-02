6a8e538ecfa07677a839466ededaa5f1106137dc
Mischa Dhar <mischadhar94@gmail.com>
2022-12-02 18:16:32 +0000

    packaged first challenge

--- challenges/Day2.py
+++ challenges/Day2.py
@@ -1,8 +1,57 @@
-def part1(data):
-    pass
+import pandas as pd
 
-def part2(data):
+from params import LOCAL_DATA_PATH
+
+data_path = LOCAL_DATA_PATH + '/Day2_data.csv'
+
+df = pd.read_csv(data_path
+            , delim_whitespace=True
+            , header=None
+            , names=['Opponent','Response'])
+
+def part1(df):
+
+    choice_dict = {'A':'Rock'
+               , 'B':'Paper'
+               ,'C':'Scissors'
+               ,'X':'Rock'
+               ,'Y':'Paper'
+               ,'Z':'Scissors'}
+
+    df = df.replace({'Opponent':choice_dict
+            ,'Response':choice_dict})
+
+    def score_calculator(row):
+
+        combo_dict = {'loss':['RockPaper','PaperScissors','ScissorsRock'],
+                    'draw':['RockRock','PaperPaper','ScissorsScissors'],
+                    'win':['PaperRock','ScissorsPaper','RockScissors']}
+
+        choice_score_dict = {'Rock':1,'Paper':2,'Scissors':3}
+        combination = row['Response'] + row['Opponent']
+
+        score = 0
+
+        if combination in combo_dict['win']:
+            score += 6 + choice_score_dict[row['Response']]
+
+        elif combination in combo_dict['draw']:
+            score += 3 + choice_score_dict[row['Response']]
+
+        elif combination in combo_dict['loss']:
+            score += 0 + choice_score_dict[row['Response']]
+
+        return score
+
+    df['Score'] = df.apply(lambda row: score_calculator(row),axis=1)
+
+    return df.Score.sum()
+
+def part2(df):
     pass
 
 if __name__ == '__main__':
-    pass
+
+    part1_output = part1(df)
+
+    print(part1_output)
