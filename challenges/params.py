4c21d1f928108e6fa932fc823ef11854096e0537
Mischa Dhar <mischadhar94@gmail.com>
2022-12-03 15:39:02 +0000

    packaged challenge 2 & fixed .env file

--- challenges/params.py
+++ challenges/params.py
@@ -4,6 +4,8 @@ import datetime
 # LOCAL_DATA_PATH = os.path.expanduser(os.environ.get("LOCAL_DATA_PATH"))
 
 LOCAL_DATA_PATH = os.environ.get("LOCAL_DATA_PATH")
+LOCAL_TOKEN_PATH = os.environ.get("TOKEN_PATH")
+
 
 #get current time
 CURRENT_DAY_OF_MONTH = datetime.datetime.now().strftime("%d")
