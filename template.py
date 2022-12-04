7623b1678b3ee3e6c7d006b3531c862d28f97164
Mischa Dhar <mischadhar94@gmail.com>
2022-12-04 10:50:16 +0000

    updated template .py file for challenges

--- template.py
+++ template.py
@@ -1,3 +1,12 @@
+from aocd import get_data
+from params import LOCAL_TOKEN_PATH
+
+with open(LOCAL_TOKEN_PATH+'token.txt') as f:
+    token = f.read().strip('\n')
+
+# Update 'get_data' params to get the desired day's data
+# data = get_data(session=token,day=,year=2022)
+
 def part1(data):
     pass
 
